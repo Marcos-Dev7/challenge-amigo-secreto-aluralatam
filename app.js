00bb282eddc7cba1b2b492c7e588888690acb191
Marcos-Dev7 <markimdvp@gmail.com>
2025-09-09 11:36:36 -0300

    feat: adicionar função adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+
+function adicionarAmigo(){
+    
+    let campoNome = document.querySelector('input');
+    let nomeAmigos = campoNome.value;
+
+    // verificando se o campo está vazio e emitindo alerta
+
+    if (nomeAmigos === ''){
+        alert("Campo vazio! Insira um nome valido!");
+    } else {
+        amigos.push(nomeAmigos);
+        campoNome.value = '';
+    }
+    console.log(amigos);
+
+};
